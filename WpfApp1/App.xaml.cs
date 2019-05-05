7b26036664fba193a11164311bd31b00b76f1824
test <test>
2019-05-05 01:16:10 -0400

    Changed App to PrismApplication

--- WpfApp1/App.xaml.cs
+++ WpfApp1/App.xaml.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Prism.Ioc;
 using System.Windows;
 
 namespace WpfApp1
@@ -11,7 +6,20 @@ namespace WpfApp1
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
-    public partial class App : Application
+    public partial class App
     {
+        protected override Window CreateShell()
+        {
+            return Container.Resolve<Shell>();
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            //containerRegistry.RegisterDialog<NotificationDialog, NotificationDialogViewModel>();
+            //containerRegistry.RegisterDialog<ConfirmationDialog, ConfirmationDialogViewModel>();
+
+            //register a custom window host
+            //containerRegistry.RegisterDialogWindow<CustomDialogWindow>();
+        }
     }
 }
