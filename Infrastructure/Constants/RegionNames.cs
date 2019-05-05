d4c9e463e71029333928f2d3ed9277f6f3c60b60
test <test>
2019-05-05 00:30:58 -0400

    Added RegionNames

---
+++ Infrastructure/Constants/RegionNames.cs
@@ -0,0 +1,10 @@
+﻿namespace Infrastructure.Constants
+{
+    public static class RegionNames
+    {
+        public static string MENU_REGION = "MenuRegion";
+        public const string SHELL_BOTTOM_REGION = "ShellBottomRegion";
+        public const string SHELL_MIDDLE_REGION = "ShellMiddleRegion";
+        public const string SHELL_TOP_REGION = "ShellTopRegion";
+    }
+}
